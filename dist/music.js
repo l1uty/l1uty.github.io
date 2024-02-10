859f6de732108d0d7864fac0cc7e71e256752713
l1uty <583747030@qq.com>
2024-02-10 21:28:21 +0800

    Site updated: 2024-02-10 21:28:21

--- dist/music.js
+++ dist/music.js
@@ -26,6 +26,66 @@ const ap = new APlayer({
         artist: '张紫豪（咚鼓版）',
         url: 'http://www.ytmp3.cn/down/61619.mp3',
         cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951163760637999&auth=603c038460ffd418f833197079ba3bd1168a4e8f',
-      },
+    },
+    {
+        name: "夏夜最后的烟火",
+        artist: '颜人中',
+        url: 'http://www.ytmp3.cn/down/78346.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951165349625690&auth=1c2a46782727950611332958510d4acfdfb5ffad',
+    },
+    {
+        name: "不在",
+        artist: '韩安旭',
+        url: 'http://www.ytmp3.cn/down/35965.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951163169037953&auth=81ca79f8fbed165b285e19b18d36504d98512ad1',
+    },
+    {
+        name: "其实都没有",
+        artist: '杨宗纬',
+        url: 'http://www.ytmp3.cn/down/39030.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951168890333304&auth=4a7b9ef5dbc27babd5898ada6f8914eecfc6cf9a',
+    },
+    {
+        name: "我的歌声里",
+        artist: '曲婉婷',
+        url: 'http://www.ytmp3.cn/down/37541.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951168588066668&auth=2947a0e6db82e6af5acf77229d257087dec7f28a',
+    },
+    {
+        name: "陷阱",
+        artist: '王北车',
+        url: 'http://www.ytmp3.cn/down/53160.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951167766703596&auth=3f64d58774631a25cd9d8acf98acbb900fedc40c',
+    },
+    {
+        name: "南风北巷",
+        artist: '邵帅',
+        url: 'http://www.ytmp3.cn/down/60853.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951165241101022&auth=4a7645548ac41b1c0cab95599c22190649b4e93d',
+    },
+    {
+        name: "寂寞烟火2023",
+        artist: '刘大拿',
+        url: 'http://www.ytmp3.cn/down/62425.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951168239829957&auth=300d6d1798d604d7b16814384a90afde2ccab004',
+    },
+    {
+        name: "哪里都是你",
+        artist: '队长',
+        url: 'http://www.ytmp3.cn/down/77653.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951162964628408&auth=dfa4ad9f877c963c9a3176af6ce314ef5615d111',
+    },
+    {
+        name: "起风了",
+        artist: '买辣椒也用券',
+        url: 'http://www.ytmp3.cn/down/78768.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951163699673355&auth=203979f0540464286450b47d29f94b1b8f1e602a',
+    },
+    {
+        name: "南山南",
+        artist: '马頔',
+        url: 'http://www.ytmp3.cn/down/34439.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=6648746813825382&auth=e809fe20a59b422e78fc8de8ed15c98fff48e7b6',
+    },
     ]
 });
