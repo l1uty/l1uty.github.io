772e3133d4d2f59451cc88a238269423ccf91d0d
l1uty <583747030@qq.com>
2024-05-05 15:41:12 +0800

    Site updated: 2024-05-05 15:41:11

--- dist/music.js
+++ dist/music.js
@@ -4,10 +4,40 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
+        name: "Kryptonite",
+        artist: 'Anson Seabra',
+        url: 'https://www.ytmp3.cn/down/78830.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951168455579476&auth=9772d8cfa56ef52462f19abf0781c4fce10dde19&quot',
+      },
+      {
+        name: "Dehors",
+        artist: 'JORDANN',
+        url: 'https://www.ytmp3.cn/down/78842.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951168356159879&auth=a8efb8d1b248759be36193775dbf528955add255&quot',
+      },
+      {
+        name: "底牌",
+        artist: 'Max李玄 / 邹沛沛',
+        url: 'https://www.ytmp3.cn/down/78834.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951169361114051&auth=a8e93ab732abdb158499e5b7f94a672657472069&quot',
+      },
+      {
         name: "这一生关于你的风景",
         artist: '隔壁老樊',
         url: 'http://www.ytmp3.cn/down/73916.mp3',
         cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951164289743850&auth=d39d8a782171d2830010f00d7530aa7b50e7dcce',
+      },
+      {
+        name: "温热蝙蝠日记Acoustic",
+        artist: '柳戈',
+        url: 'https://www.ytmp3.cn/down/78824.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951164946651062&auth=0d4bcd607ce4f2c67b4cae87c22330b6086067e6&quot',
+      },
+      {
+        name: "触碰不到的你",
+        artist: '余佳运',
+        url: 'https://www.ytmp3.cn/down/78820.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951165085408736&auth=cedf55ec7f369e0d6927bc5d722a05d475069399&quot',
       },
     {
         name: "如果这就是爱情",
@@ -22,10 +52,10 @@ const ap = new APlayer({
         cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951164485969446&auth=ba0fb57c0a20399a42d5ca4164c69abaa00aad2f&quot',
     },
     {
-        name: "可不可以",
-        artist: '张紫豪（咚鼓版）',
-        url: 'http://www.ytmp3.cn/down/61619.mp3',
-        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951163760637999&auth=603c038460ffd418f833197079ba3bd1168a4e8f',
+        name: "I miss your Christmas",
+        artist: 'Kid Travis',
+        url: 'https://www.ytmp3.cn/down/78836.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951166647105460&auth=f196368054ddcacc6d428e96c98acb74d33a922a&quot',
     },
     {
         name: "夏夜最后的烟火",
@@ -34,10 +64,10 @@ const ap = new APlayer({
         cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951165349625690&auth=1c2a46782727950611332958510d4acfdfb5ffad',
     },
     {
-        name: "不在",
-        artist: '韩安旭',
-        url: 'http://www.ytmp3.cn/down/35965.mp3',
-        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951163169037953&auth=81ca79f8fbed165b285e19b18d36504d98512ad1',
+        name: "背叛",
+        artist: '呆呆破',
+        url: 'https://www.ytmp3.cn/down/78835.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951168487800533&auth=098c7350455074798c8a66d6417d69006ba2c735&quot',
     },
     {
         name: "其实都没有",
@@ -46,22 +76,28 @@ const ap = new APlayer({
         cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951168890333304&auth=4a7b9ef5dbc27babd5898ada6f8914eecfc6cf9a',
     },
     {
-        name: "我的歌声里",
-        artist: '曲婉婷',
-        url: 'http://www.ytmp3.cn/down/37541.mp3',
-        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951168588066668&auth=2947a0e6db82e6af5acf77229d257087dec7f28a',
+        name: "3:00 AM",
+        artist: 'Finding Hope',
+        url: 'https://www.ytmp3.cn/down/78832.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951168785963692&auth=ba77691b12e261e0540d8c11fec1a2bca80b3016&quot',
     },
     {
-        name: "陷阱",
-        artist: '王北车',
-        url: 'http://www.ytmp3.cn/down/53160.mp3',
-        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951167766703596&auth=3f64d58774631a25cd9d8acf98acbb900fedc40c',
+        name: "Matsuri",
+        artist: 'Will Stetson',
+        url: 'https://www.ytmp3.cn/down/78828.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951168502826457&auth=babec8584b56e6920495a114f561578caeb4d721&quot',
     },
     {
         name: "南风北巷",
         artist: '邵帅',
         url: 'http://www.ytmp3.cn/down/60853.mp3',
         cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951165241101022&auth=4a7645548ac41b1c0cab95599c22190649b4e93d',
+    },
+    {
+        name: "余香",
+        artist: '张小九',
+        url: 'https://www.ytmp3.cn/down/76643.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951165221750422&auth=882321b58a92788abe174c4a03dac44527e56612&quot',
     },
     {
         name: "寂寞烟火2023",
@@ -76,16 +112,22 @@ const ap = new APlayer({
         cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951162964628408&auth=dfa4ad9f877c963c9a3176af6ce314ef5615d111',
     },
     {
-        name: "起风了",
-        artist: '买辣椒也用券',
-        url: 'http://www.ytmp3.cn/down/78768.mp3',
-        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951163699673355&auth=203979f0540464286450b47d29f94b1b8f1e602a',
+        name: "u hurt me hurts u",
+        artist: 'Anson Seabra',
+        url: 'https://www.ytmp3.cn/down/78827.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951166520899559&auth=d16a963836bd6a97c7dfb726990ecc77de17eb18&quot',
     },
     {
         name: "南山南",
         artist: '马頔',
         url: 'http://www.ytmp3.cn/down/34439.mp3',
         cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=6648746813825382&auth=e809fe20a59b422e78fc8de8ed15c98fff48e7b6',
+    },
+    {
+        name: "Broken Boy",
+        artist: 'Anson Seabra',
+        url: 'https://www.ytmp3.cn/down/78825.mp3',
+        cover: 'https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951168606179052&auth=390e3b9f32896b74e5306c7ce354d7538ae59d3c&quot',
     },
     ]
 });
